97cb7d4d92169e38eb146b30cd885c3fc02b972e
Prateek <2018csb1114@iitrpr.ac.in>
2021-04-12 11:33:04 +0530

    Forget Password Added

--- app/src/main/java/com/example/pgidoctor/LoginActivity.kt
+++ app/src/main/java/com/example/pgidoctor/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.example.pgidoctor
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.util.Patterns
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
@@ -14,6 +16,8 @@ import android.widget.Toast
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.change_password_layout.view.*
+import kotlinx.android.synthetic.main.forgot_password.view.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +38,39 @@ class LoginActivity : AppCompatActivity() {
         val loginButton: Button = findViewById(R.id.login_button)
 
         val loginProgress: ProgressBar = findViewById(R.id.login_progress)
-
+        val forgot_Password : TextView = findViewById(R.id.forgot_password)
+        forgot_Password.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.forgot_password,null);
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+//                .setTitle("Change Password")
+            val mAlertDialog = mBuilder.show()
+            mDialogView.submit_button.setOnClickListener {
+                mAlertDialog.dismiss()
+                val emaill = mDialogView.enter_email.editText?.text.toString()
+                if(mDialogView.enter_email.editText?.text.toString().isNotEmpty()){
+                    FirebaseAuth.getInstance().sendPasswordResetEmail(emaill)
+                        .addOnCompleteListener{ task ->
+                            if(task.isSuccessful) {
+                                Toast.makeText(
+                                    this,
+                                    "Email sent successfully to reset your password ! ",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            } else{
+                                Toast.makeText(
+                                    this,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                }
+                else{
+                    Toast.makeText(this, "Please enter your email id", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
         loginButton.setOnClickListener {
             val email = emailText.editText?.text.toString()
             val password = passwordText.editText?.text.toString()
